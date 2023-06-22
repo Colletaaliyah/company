983dbb6fd6a16ddf69f3f22125e4fac7c5a4e0bc
Aaliyah Collet <colletaaliyah1@gmail.com>
2023-06-23 01:45:48 +0300

    customerstableupdated

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
-from flask import Flask,render_template
-import psycopg2
+from flask import Flask,render_template,request,redirect
+from mytestpostgres import fetch_data
+from mytestpostgres import insert_customers
+
+
+
 app=Flask(__name__)
 
 #Connect to an existing database
-conn = psycopg2.connect(user="postgres", password="Candidcoco@20", host="localhost",
-                         port="5432", database="Company")
+
 # a connection creates a session.
 #a session is the period in which you’re  connected or logged in into your db. 
 #Open a cursor to perform database operations
-cur = conn.cursor()
+
+
 
 
 
@@ -18,13 +22,37 @@ def hello_world():
     name='aaliyah'
     return render_template('index.html',name=name)
 
+# @app.route('/customers')
+# def customers():
+#      cur.execute('select * from customers')
+#      customers=cur.fetchall()
+#      print(customers)
+#      return render_template("customers.html",customers=customers)
+
+
 @app.route('/customers')
 def customers():
-     cur.execute('select * from customers')
-     customers=cur.fetchall()
-     print(customers)
-     return render_template("customers.html",customers=customers)
+    customers=fetch_data('customers')
+    return render_template("customers.html",customers=customers)
 
 
-if __name__=="__main__":
-    app.run()
+
+@app.route("/addcustomers",methods=['POST','GET'])
+def addcustomers():
+    if request.method=='POST':
+        first_name=request.form["first_name"]
+        last_name=request.form["last_name"]
+        email=request.form['email']
+        phone=request.form["phone"]
+        print(first_name)
+        print(last_name)
+        print(email)
+        print(phone)
+        customer=(first_name,last_name,email,phone)
+        insert_customers(customer)
+        return redirect("/customers")
+
+
+
+# if __name__=="__main__":
+app.run(debug=True)
---
+++ mytestpostgres.py
@@ -0,0 +1,67 @@
+import psycopg2
+try:
+# Connect to your postgres DB
+    conn = psycopg2.connect("dbname=Company user=postgres password=Candidcoco@20")
+
+    # Open a cursor to perform database operations
+    cur = conn.cursor()
+except Exception as e:
+    print(e)
+
+def fetch_data(tbln):
+    try:
+        q="SELECT * FROM " + tbln + ';'
+        cur.execute(q)
+        records = cur.fetchall()
+        return records
+    except Exception as e:
+        return e
+    
+def insert_product(v):
+        vs=str(v)
+        q= "INSERT INTO products(name, buying_price, selling_price, quantity)"\
+            "VALUES" + vs
+        cur.execute(q)
+        conn.commit()
+        return q
+
+
+def insert_sales(v):
+        vs=str(v)
+        q= "INSERT INTO sales(pid, quantity,created_at)"\
+            "VALUES" + vs
+        cur.execute(q)
+        conn.commit()
+        return q
+
+
+def insert_customers(v):
+        vs=str(v)
+        q= "INSERT INTO customers(first_name, last_name,email,phone)"\
+            "VALUES" + vs
+        cur.execute(q)
+        conn.commit()
+        return q
+
+
+
+
+
+
+# p1=insert_product(('popcorn',50,50,30))
+# print(p1)
+# p1=insert_product(('broccoli',40,60,10))
+# print(p1)
+
+# data = fetch_data("products")
+# print(data)
+# Execute a query
+# cur.execute("SELECT * FROM products")
+
+# Retrieve query results
+# records = cur.fetchall()
+
+# print(records)
+#store all product names in alist and only print that list. do not filter in the query.
+# newlist=[i[1] for i in records]
+# print(newlist)
